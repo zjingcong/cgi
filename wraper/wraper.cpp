a787696c30932f0fcffc65407f0507cee3a65ee9
zjingcong <jingcong.zhang@outlook.com>
2016-10-24 18:53:00 -0400

    Add bilinear and interactive with bugs.

--- wraper/wraper.cpp
+++ wraper/wraper.cpp
@@ -24,10 +24,332 @@ OIIO_NAMESPACE_USING
 # define min(x, y) (x < y ? x : y)
 
 static Matrix3D transMatrix;  // transform matrix for the entire transform
+static Matrix3D translation;  // extra translation transform matrix
 static unsigned char *inputpixmap;  // input image pixels pixmap
 static unsigned char *outputpixmap; // output image pixels pixmap
+static string inputImage;  // input image file name
+static string outputImage; // output image file name
 static int xres, yres;  // input image size: width, height
 static int xres_out, yres_out;  // output image size: width, height
+static int mode;  // program mode - 0: projective wrap (basic requirement), 1: bilinear wrap, 2: interactive mode
+static Vector2D mouseClickCorners[4];
+static int mouse_index = 0;
+
+
+/*
+command line option parser
+wraper input_image_name [output_image_name] [mode]
+default mode: projective mode
+mode switch:
+  -b          bilinear switch - do the bilinear warp instead of a perspective warp
+  -i          interactive switch
+matrix commands:
+  r theta     counter clockwise rotation about image origin, theta in degrees
+  s sx sy     scale (watch out for scale by 0!)
+  t dx dy     translate
+  f xf yf     flip - if xf = 1 flip horizontal, yf = 1 flip vertical
+  h hx hy     shear
+  p px py     perspective
+  d           done
+*/
+void helpPrinter()
+{
+  // print help message
+  cout << "Help: " << endl;
+  cout << "[Usage] wraper input_image_name [output_image_name] [mode]" << endl;
+  cout << "--------------------------------------------------------------" << endl;
+  cout << "default mode: projective wrap" << endl;
+  cout << "mode switch: " << endl;
+  cout << "\t-b          bilinear switch - do the bilinear warp instead of a perspective warp\n"
+       << "\t-i          interactive switch" << endl;
+  cout << "matrix commands: " << endl;
+  cout << "\tr theta     counter clockwise rotation about image origin, theta in degrees\n"
+       << "\ts sx sy     scale (watch out for scale by 0!)\n"
+       << "\tt dx dy     translate\n"
+       << "\tf xf yf     flip - if xf = 1 flip horizontal, yf = 1 flip vertical\n"
+       << "\th hx hy     shear\n"
+       << "\tp px py     perspective\n"
+       << "\td           done\n" << endl;
+}
+char **getIter(char** begin, char** end, const std::string& option) {return find(begin, end, option);}
+void getCmdOptions(int argc, char **argv, string &inputImage, string &outputImage)
+{
+  // print help message and exit the program
+  if (argc < 2)
+  {
+    helpPrinter();
+    exit(0);
+  }
+  mode = 0;
+  char **iter = getIter(argv, argv + argc, "-b");
+  if (iter != argv + argc)  {mode = 1;  cout << "program mode: bilinear wrap" << endl;}
+  else
+  {
+    iter = getIter(argv, argv + argc, "-i");
+    if (iter != argv + argc)  {mode = 2;  cout << "program mode: interactive" << endl;}
+  }
+  inputImage = argv[1];
+  if (mode == 0)
+  {
+    cout << "program mode: projective wrap" << endl;
+    if (argc == 3)  {outputImage = argv[2];}
+  }
+  else
+  {
+    if (argc == 4)  {outputImage = argv[2];}
+  }
+}
+
+
+/*
+matrix command parser
+  calculate forward transform matrix
+*/
+void generateMatrix()
+{
+  Matrix3D xform;
+  char tag;
+  cin >> tag;
+  while (tag != 'd')
+  {
+    if (tag != 'r' && tag != 's' && tag != 't' && tag != 'f' && 
+        tag != 'h' && tag != 'p' && tag != 'd') 
+    {helpPrinter(); exit(0);}
+    // generate transform matrix
+    switch (tag)
+    {
+      // rotation
+      case 'r':
+        double theta;
+        cin >> theta;
+        xform[0][0] = xform[1][1] = cos(theta * M_PI / 180);
+        xform[0][1] = -sin(theta * M_PI / 180);
+        xform[1][0] = sin(theta * M_PI / 180);
+        break;
+      // scale
+      case 's':
+        double sx, sy;
+        cin >> sx >> sy;
+        xform[0][0] = sx;
+        xform[1][1] = sy;
+        break;
+      // translate
+      case 't':
+        double dx, dy;
+        cin >> dx >> dy;
+        xform[0][2] = dx;
+        xform[1][2] = dy;
+        break;
+      // flip: if xf = 1 flip horizontal, yf = 1 flip vertical
+      case 'f':
+        double xf, yf;
+        cin >> xf >> yf;
+        if (xf == 1)  {xform[0][0] = -1;}
+        if (yf == 1)  {xform[1][1] = -1;}
+        break;
+      // shear
+      case 'h':
+        double hx, hy;
+        cin >> hx >> hy;
+        xform[0][1] = hx;
+        xform[1][0] = hy;
+        break;
+      // perspective
+      case 'p':
+        double px, py;
+        cin >> px >> py;
+        xform[2][0] = px;
+        xform[2][1] = py;
+        break;
+      default:
+        return;
+    }
+    cin >> tag;
+  }
+  transMatrix = xform * transMatrix;
+  cout << "transform matrix: " << endl;
+  transMatrix.print();
+}
+
+
+/*
+four corners forward wrap to make space for output image pixmap
+*/
+void boundingbox(Vector2D xycorners[])
+{
+  Vector2D u0, u1, u2, u3;
+  u0.x = 0;
+  u0.y = 0;
+  u1.x = xres;
+  u1.y = 0;
+  u2.x = 0;
+  u2.y = yres;
+  u3.x = xres;
+  u3.y = yres;
+
+  //Vector2D xy0, xy1, xy2, xy3;
+  xycorners[0] = transMatrix * u0;
+  xycorners[1] = transMatrix * u1;
+  xycorners[2] = transMatrix * u2;
+  xycorners[3] = transMatrix * u3;
+
+  double x0, y0, x1, y1, x2, y2, x3, y3, x_min, x_max, y_min, y_max;
+  x0 = xycorners[0].x;
+  y0 = xycorners[0].y;
+  x1 = xycorners[1].x;
+  y1 = xycorners[1].y;
+  x2 = xycorners[2].x;
+  y2 = xycorners[2].y;
+  x3 = xycorners[3].x;
+  y3 = xycorners[3].y;
+  
+  // calculate output image size
+  x_max = max(max(x0, x1), max(x2, x3));
+  x_min = min(min(x0, x1), min(x2, x3));
+  y_max = max(max(y0, y1), max(y2, y3));
+  y_min = min(min(y0, y1), min(y2, y3));
+  xres_out = ceil(x_max - x_min);
+  yres_out = ceil(y_max - y_min);
+  cout << "output image size: " << xres_out << "x" << yres_out << endl;
+  
+  // calculate extra translation
+  // extra translation transform: set x_min and y_min to 0
+  translation[0][2] = 0 - x_min;
+  translation[1][2] = 0 - y_min;
+}
+
+
+/*
+projective wrap inverse map
+*/
+void inversemap()
+{
+  outputpixmap = new unsigned char [xres_out * yres_out * 4];
+  // fill the output image with a clear transparent color(0, 0, 0, 0)
+  for (int i = 0; i < xres_out * yres_out * 4; i++) {outputpixmap[i] = 0;}
+
+  Matrix3D invMatrix;
+  transMatrix = translation * transMatrix;
+  invMatrix = transMatrix.inverse();
+  cout << "inverse matrix: " << endl;
+  invMatrix.print();
+
+  for (int row_out = 0; row_out < yres_out; row_out++)  // output image row
+  {
+    for (int col_out = 0; col_out < xres_out; col_out++)  // output image col
+    {
+      // output coordinate
+      Vector2D xy;
+      xy.x = col_out + 0.5;
+      xy.y = row_out + 0.5;
+
+      // inverse mapping
+      double u, v;
+      u = (invMatrix * xy).x;
+      v = (invMatrix * xy).y;
+      int row_in, col_in;
+      row_in = floor(v);
+      col_in = floor(u);
+      
+      if (row_in < yres && row_in >= 0 && col_in < xres && col_in >= 0)
+      {
+        for (int k = 0; k < 4; k++)
+        {outputpixmap[(row_out * xres_out + col_out) * 4 + k] = inputpixmap[(row_in * xres + col_in) * 4 + k];}
+      }
+    }
+  }
+  cout << "projective inverse complete." << endl;
+}
+
+
+/*
+bilinear wrap inverse map
+*/
+void bilinear(Vector2D xycorners[])
+{
+  outputpixmap = new unsigned char [xres_out * yres_out * 4];
+  // fill the output image with a clear transparent color(0, 0, 0, 0)
+  for (int i = 0; i < xres_out * yres_out * 4; i++) {outputpixmap[i] = 0;}
+  
+  cout << "corners: " << endl;
+  for (int i = 0; i < 4; i++) {cout << xycorners[i].x << " " << xycorners[i].y << endl;}
+  for (int i = 0; i < 4; i++) {xycorners[i] = translation * xycorners[i];}
+  cout << "----------------------" << endl;
+  cout << "new corners: " << endl;
+  for (int i = 0; i < 4; i++) {cout << xycorners[i].x << " " << xycorners[i].y << endl;}
+
+  for (int row_out = 0; row_out < yres_out; row_out++)
+  {
+    for (int col_out = 0; col_out < xres_out; col_out++)
+    {
+      Vector2D xy, uv;
+      BilinearCoeffs coeff;
+
+      xy.x = col_out + 0.5;
+      xy.y = row_out + 0.5;
+      // xy = translation.inverse() * xy;
+      setbilinear(xres_out, yres_out, xycorners, coeff);
+      invbilinear(coeff, xy, uv);
+
+      int row_in, col_in;
+      row_in = floor(uv.y);
+      col_in = floor(uv.x);
+
+      if (row_in < yres && row_in >= 0 && col_in < xres && col_in >= 0)
+      {
+        for (int k = 0; k < 4; k++)
+        {outputpixmap[(row_out * xres_out + col_out) * 4 + k] = inputpixmap[(row_in * xres + col_in) * 4 + k];}
+      }
+    }
+  }
+  cout << "bilinear inverse complete." << endl;
+}
+
+
+void interactive()
+{
+  // interMatrix
+  // | a b c ||u|   |x|
+  // | d e f ||v| = |y|
+  // | g h 1 ||1|   |1|
+  // a = (x1 - x0) / width, b = (x3 - x1) / height, c = x0, d = (y1 - y0) / w0
+  // e = (y3 - y1) / height, f = y0, g = 0, h = 0
+  Matrix3D interMatrix;
+  interMatrix[0][0] = (mouseClickCorners[1].x - mouseClickCorners[0].x) / double(xres);
+  interMatrix[0][1] = (mouseClickCorners[3].x - mouseClickCorners[1].x) / double(yres);
+  interMatrix[0][2] = mouseClickCorners[0].x;
+  interMatrix[1][0] = (mouseClickCorners[1].y - mouseClickCorners[0].y) / double(xres);
+  interMatrix[1][1] = (mouseClickCorners[3].y - mouseClickCorners[1].y) / double(yres);
+  interMatrix[1][2] = mouseClickCorners[0].y;
+
+  // inverse matrix
+  Matrix3D invinterMatrix = interMatrix.inverse();
+  for (int row_out = 0; row_out < yres_out; row_out++)  // output image row
+  {
+    for (int col_out = 0; col_out < xres_out; col_out++)  // output image col
+    {
+      // output coordinate
+      Vector2D xy;
+      xy.x = col_out + 0.5;
+      xy.y = row_out + 0.5;
+
+      // inverse mapping
+      double u, v;
+      u = (invinterMatrix * xy).x;
+      v = (invinterMatrix * xy).y;
+      int row_in, col_in;
+      row_in = floor(v);
+      col_in = floor(u);
+      
+      if (row_in < yres && row_in >= 0 && col_in < xres && col_in >= 0)
+      {
+        for (int k = 0; k < 4; k++)
+        {outputpixmap[(row_out * xres_out + col_out) * 4 + k] = inputpixmap[(row_in * xres + col_in) * 4 + k];}
+      }
+    }
+  }
+  cout << "interactive complete." << endl;
+}
 
 
 /*
@@ -164,11 +486,46 @@ void displayOutput()  {display(outputpixmap, xres_out, yres_out);}
 
 
 /*
-mouse callback: left click any of the displayed windows to quit
+Keyboard Callback Routine: 'q', 'Q' or ESC quit
+This routine is called every time a key is pressed on the keyboard
+*/
+void handleKey(unsigned char key, int x, int y)
+{
+  switch(key)
+  {
+    case 'q':		// q - quit
+    case 'Q':
+    case 27:		// esc - quit
+      exit(0);
+      
+    default:		// not a valid key -- just ignore it
+      return;
+  }
+}
+
+
+/*
+mouse callback
+  left click on the output image window to get cornet positions
 */
 void mouseClick(int button, int state, int x, int y)
 {
-  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {exit(0);}
+  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+  {
+    if (mouse_index < 4)
+    {
+      cout << "point" << mouse_index << ": ("<< x << ", " << y << ")" << endl;
+      mouseClickCorners[mouse_index].x = x;
+      mouseClickCorners[mouse_index].y = y;
+    }
+    mouse_index++;
+    if (mouse_index == 4)
+    {
+      interactive();
+      glutPostRedisplay();
+      if (outputImage != "")  {writeimage(outputImage);}
+    }
+  }
 }
 
 
@@ -203,213 +560,53 @@ void handleReshape_out(int w, int h)  {handleReshape(w, h, xres_out, yres_out);}
 
 
 /*
-command line option parser
-wraper input_image_name [output_image_name](optional)
-  r theta     counter clockwise rotation about image origin, theta in degrees
-  s sx sy     scale (watch out for scale by 0!)
-  t dx dy     translate
-  f xf yf     flip - if xf = 1 flip horizontal, yf = 1 flip vertical
-  h hx hy     shear
-  p px py     perspective
-  d           done 
-*/
-void helpPrinter()
-{
-  // print help message
-  cout << "Help: " << endl;
-  cout << "[Usage] wraper input_image_name [output_image_name](optional)" << endl;
-  cout << "--------------------------------------------------------------" << endl;
-  cout << "\tr theta     counter clockwise rotation about image origin, theta in degrees\n"
-       << "\ts sx sy     scale (watch out for scale by 0!)\n"
-       << "\tt dx dy     translate\n"
-       << "\tf xf yf     flip - if xf = 1 flip horizontal, yf = 1 flip vertical\n"
-       << "\th hx hy     shear\n"
-       << "\tp px py     perspective\n"
-       << "\td           done\n" << endl;
-}
-void getCmdOptions(int argc, char **argv, string &inputImage, string &outputImage)
-{
-  // print help message and exit the program
-  if (argc < 2)
-  {
-    helpPrinter();
-    exit(0);
-  }
-  inputImage = argv[1];
-  if (argc == 3) {outputImage = argv[2];}
-}
-
-
-/*
-calculate forward transform matrix
-*/
-void generateMatrix()
-{
-  Matrix3D xform;
-  char tag;
-  cin >> tag;
-  while (tag != 'd')
-  {
-    if (tag != 'r' && tag != 's' && tag != 't' && tag != 'f' && 
-        tag != 'h' && tag != 'p' && tag != 'd') 
-    {helpPrinter(); exit(0);}
-    // generate transform matrix
-    switch (tag)
-    {
-      // rotation
-      case 'r':
-        double theta;
-        cin >> theta;
-        xform[0][0] = xform[1][1] = cos(theta * M_PI / 180);
-        xform[0][1] = -sin(theta * M_PI / 180);
-        xform[1][0] = sin(theta * M_PI / 180);
-        break;
-      // scale
-      case 's':
-        double sx, sy;
-        cin >> sx >> sy;
-        xform[0][0] = sx;
-        xform[1][1] = sy;
-        break;
-      // translate
-      case 't':
-        double dx, dy;
-        cin >> dx >> dy;
-        xform[0][2] = dx;
-        xform[1][2] = dy;
-        break;
-      // flip: if xf = 1 flip horizontal, yf = 1 flip vertical
-      case 'f':
-        double xf, yf;
-        cin >> xf >> yf;
-        if (xf == 1)  {xform[0][0] = -1;}
-        if (yf == 1)  {xform[1][1] = -1;}
-        break;
-      // shear
-      case 'h':
-        double hx, hy;
-        cin >> hx >> hy;
-        xform[0][1] = hx;
-        xform[1][0] = hy;
-        break;
-      // perspective
-      case 'p':
-        double px, py;
-        cin >> px >> py;
-        xform[2][0] = px;
-        xform[2][1] = py;
-        break;
-      default:
-        return;
-    }
-    cin >> tag;
-  }
-  transMatrix = xform * transMatrix;
-  cout << "transform matrix: " << endl;
-  transMatrix.print();
-}
-
-
-/*
-four corner forward wrap to make space for output image pixmap
-*/
-void boundingbox()
-{
-  Vector2D u0, u1, u2, u3;
-  u0.x = 0;
-  u0.y = 0;
-  u1.x = xres;
-  u1.y = 0;
-  u2.x = 0;
-  u2.y = yres;
-  u3.x = xres;
-  u3.y = yres;
-
-  double x0, y0, x1, y1, x2, y2, x3, y3, x_min, x_max, y_min, y_max;
-  x0 = (transMatrix * u0).x;
-  y0 = (transMatrix * u0).y;
-  x1 = (transMatrix * u1).x;
-  y1 = (transMatrix * u1).y;
-  x2 = (transMatrix * u2).x;
-  y2 = (transMatrix * u2).y;
-  x3 = (transMatrix * u3).x;
-  y3 = (transMatrix * u3).y;
-  
-  x_max = max(max(x0, x1), max(x2, x3));
-  x_min = min(min(x0, x1), min(x2, x3));
-  y_max = max(max(y0, y1), max(y2, y3));
-  y_min = min(min(y0, y1), min(y2, y3));
-  xres_out = ceil(x_max - x_min);
-  yres_out = ceil(y_max - y_min);
-  
-  cout << "output image size: " << xres_out << "x" << yres_out << endl;
-  cout << x_min << " " << y_min << endl;
-
-  // extra transform: set x_min and y_min to 0
-  Matrix3D translation;
-  translation[0][2] = 0 - x_min;
-  translation[1][2] = 0 - y_min;
-  transMatrix = translation * transMatrix;
-  cout << "transform matrix with extra translation: " << endl;
-  transMatrix.print();
-}
-
-
-void inversemap()
-{
-  outputpixmap = new unsigned char [xres_out * yres_out * 4];
-  // fill the output image with a clear transparent color(0, 0, 0, 0)
-  for (int i = 0; i < xres_out * yres_out * 4; i++) {outputpixmap[i] = 0;}
-
-  Matrix3D invMatrix;
-  invMatrix = transMatrix.inverse();
-
-  for (int row_out = 0; row_out < yres_out; row_out++)  // output image row
-  {
-    for (int col_out = 0; col_out < xres_out; col_out++)  // output image col
-    {
-      // output coordinate
-      Vector2D xx;
-      xx.x = col_out + 0.5;
-      xx.y = row_out + 0.5;
-
-      // inverse mapping
-      double u, v;
-      u = (invMatrix * xx).x;
-      v = (invMatrix * xx).y;
-      int row_in, col_in;
-      row_in = floor(v);
-      col_in = floor(u);
-      
-      if (row_in < yres && row_in >= 0 && col_in < xres && col_in >= 0)
-      {
-        for (int k = 0; k < 4; k++)
-        {outputpixmap[(row_out * xres_out + col_out) * 4 + k] = inputpixmap[(row_in * xres + col_in) * 4 + k];}
-      }
-    }
-  }
-}
-
-
-/*
 Main program
 */
 int main(int argc, char* argv[])
 {
-  string inputImage;  // input image file name
-  string outputImage; // output image file name
+  Vector2D xycorners[4];  // output image corner positions
 
   // command line parser and calculate transform matrix
   getCmdOptions(argc, argv, inputImage, outputImage);
   // read input image
   readimage(inputImage);
-  // calculate transform matrix
-  generateMatrix();
-  boundingbox();
-  inversemap();
-  cout << "inverse complete." << endl;
-  // write out to an output image file
-  if (outputImage != "") {writeimage(outputImage);}
+
+  // inverse map
+  switch (mode)
+  {
+    // projective wrap
+    case 0:
+      // calculate transform matrix
+      generateMatrix();
+      // get four output corner positions
+      boundingbox(xycorners);
+      inversemap();
+      if (outputImage != "") {writeimage(outputImage);}
+      break;
+
+    // bilinear wrap
+    case 1:
+      // calculate transform matrix
+      generateMatrix();
+      // get four output corner positions
+      boundingbox(xycorners);
+      bilinear(xycorners);
+      if (outputImage != "") {writeimage(outputImage);}
+      break;
+
+    // interactive
+    case 2:
+      xres_out = xres;
+      yres_out = yres;
+      outputpixmap = new unsigned char [xres_out * yres_out * 4];
+      // fill the output image with a clear transparent color(0, 0, 0, 0)
+      for (int i = 0; i < xres_out * yres_out * 4; i++) {outputpixmap[i] = 0;}
+      cout << "Left click the mouse in the output window to position 4 corners for output image." << endl;
+      break;
+
+    default:
+      return 0;
+  }
   
   // display input image and output image in seperated windows
   // start up the glut utilities
@@ -423,16 +620,17 @@ int main(int argc, char* argv[])
   glutCreateWindow("Input Image");  
   // set up the callback routines to be called when glutMainLoop() detects an event
   glutDisplayFunc(displayInput);	  // display callback
-  glutMouseFunc(mouseClick);  // mouse callback
+  glutKeyboardFunc(handleKey);	  // keyboard callback
   glutReshapeFunc(handleReshape_in); // window resize callback
   
   // display the output image
   // second window: output image
   glutInitWindowSize(xres_out, yres_out);
-  glutCreateWindow("Wrapped Image");
+  glutCreateWindow("Output Image");
   // set up the callback routines to be called when glutMainLoop() detects an event
   glutDisplayFunc(displayOutput);	  // display callback
-  glutMouseFunc(mouseClick);  // mouse callback
+  glutKeyboardFunc(handleKey);	  // keyboard callback
+  if (mode == 2)  {glutMouseFunc(mouseClick);}  // mouse callback
   glutReshapeFunc(handleReshape_out); // window resize callback
   
   // Routine that loops forever looking for events. It calls the registered
